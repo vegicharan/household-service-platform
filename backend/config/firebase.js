17119e0bba685c02d2c404aaf70225626b5e4ee4
Charan <vegicharan2003@gmail.com>
2026-01-29 01:28:07 +0530

    Use Firebase credentials from environment variable for Render

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,8 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
